a47f9d329aa4bfad9087d904fd6eaf7197cf8ff7
Greg Magarshak <greg@qbix.com>
2024-10-06 00:03:21 -0400

    Renamed install script from mysql to local

--- scripts/Streams/1.2.6.1-Streams.local.php
+++ scripts/Streams/1.2.6.1-Streams.local.php
@@ -1,14 +1,13 @@
 <?php
 	
-function Streams_1_2_6_1_Streams()
+function Streams_1_2_6_1_Streams_local()
 {
 	echo "Make symlinks for Streams/icons/Streams/video".PHP_EOL;
 
 	$dir = STREAMS_PLUGIN_FILES_DIR.'/Streams/icons/Streams/video/';
-
-	foreach ([40, 50, 80, 200, 400] as $size) {
+	foreach (array(40, 50, 80, 200, 400) as $size) {
 		Q_Utils::symlink($dir.$size.'.png', $dir.$size.'x.png');
 	}
 }
 
-Streams_1_2_6_1_Streams();
+Streams_1_2_6_1_Streams_local();
