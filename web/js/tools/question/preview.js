9d58f8a632051dddf641e3110e5d5af6e4a00f40
Greg Magarshak <greg@qbix.com>
2024-11-05 19:56:36 -0500

    Streams/question/preview answer tools

--- web/js/tools/question/preview.js
+++ web/js/tools/question/preview.js
@@ -147,7 +147,7 @@ Q.Tool.define("Streams/question/preview", ["Streams/preview"], function _Streams
 						answerTool.updateContent(loggedInUserId, response.slots.content);
 
 						var answerTools = tool.children('Streams/answer/preview');
-						for (var k in children) {
+						for (var k in answerTools) {
 							answerTools[k].stream.refresh(function (err) {
 								if (err) {
 									return;
