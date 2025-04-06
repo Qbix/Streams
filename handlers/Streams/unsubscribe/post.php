79925c5d6624426f5f215e4cb75be526430ce4db
Greg Magarshak <greg@qbix.com>
2025-04-06 00:37:33 -0400

    If user has custom icon, still show group photo if exists

--- handlers/Streams/unsubscribe/post.php
+++ handlers/Streams/unsubscribe/post.php
@@ -6,7 +6,9 @@ function Streams_unsubscribe_post()
 	$publisherId = Streams::requestedPublisherId();
 	$streamName = Streams::requestedName(true);
 	$stream = Streams_Stream::fetch($user->id, $publisherId, $streamName, true);
-	if ($participant = $stream->unsubscribe($participant)) {
+	if ($participant = $stream->unsubscribe()) {
 		Q_Response::setSlot('participant', $participant->exportArray());
+	} else {
+		Q_Response::setSlot('participant', null);
 	}
 }
