6400d8dbdc271f6e568b5d96f977d929eaac1e7a
Greg Magarshak <greg@qbix.com>
2025-11-16 23:38:34 -0500

    Fixed and improved Streams/access tool php

--- handlers/Streams/access/tool.php
+++ handlers/Streams/access/tool.php
@@ -13,12 +13,38 @@
  * @param {string} [$options.streamName] the name of the stream for which to edit access levels
  * @param {array} [$options.tabs] Defaults to ("read", "write", "admin"), but you can pass a subset.
  * @param {array} [$options.ranges] associative array with keys "read", "write", "admin" and values as associative arrays of ($min, $max) for the displayed levels.
+ * @param {array} [$options.filter] associative array with keys "read", "write", "admin" and values as arrays of level integers for which there are labels
  * @param {boolean} [$options.controls] optionally set this to true to render only the controls
  */
 function Streams_access_tool($options)
 {
+	extract($options);
+	$user = Users::loggedInUser(true);
+
+	/**
+	 * @var string $streamName
+	 */
+	if (empty($streamName)) {
+		$streamName = Streams::requestedName(true);
+	}
+
+	if (empty($publisherId)) {
+		$publisherId = Streams::requestedPublisherId();
+		if (empty($publisherId)) {
+			$publisherId = $user->id;
+		}
+	}
+	$stream = Streams_Stream::fetch($user->id, $publisherId, $streamName);
+    if (!$stream) {
+        throw new Q_Exception_MissingRow(array(
+            'table' => 'stream',
+            'criteria' => 'with that name'
+        ));
+	}
+	$stream->addPreloaded($user->id);
+
 	$defaults = Streams_Stream::getConfigField(
-		$streamType, array('access'), array()
+		$stream->type, array('access'), array()
 	);
 	$options = array_merge($defaults, $options);
 
@@ -33,32 +59,11 @@ function Streams_access_tool($options)
 	);
 	$tabs = Q::take($tabs, $tabNames);
 
-	$user = Users::loggedInUser(true);
-	/**
-	 * @var string $streamName
-	 */
-	if (empty($streamName)) {
-		$streamName = Streams::requestedName(true);
-	}
-
-	if (empty($publisherId)) {
-		$publisherId = Streams::requestedPublisherId();
-		if (empty($publisherId)) {
-			$publisherId = $user->id;
-		}
-	}
-
 	reset($tabs);
 	$tab = Q::ifset($_REQUEST, 'tab', key($tabs));
-
-	$stream = Streams_Stream::fetch($user->id, $publisherId, $streamName);
-    if (!$stream) {
-        throw new Q_Exception_MissingRow(array(
-            'table' => 'stream',
-            'criteria' => 'with that name'
-        ));
+	if (!isset($tabs[$tab])) {
+		$tab = key($tabs);
 	}
-	$stream->addPreloaded($user->id);
 
 	if (!$stream->testAdminLevel('own')) {
 		throw new Users_Exception_NotAuthorized();
@@ -102,8 +107,8 @@ function Streams_access_tool($options)
 			break;
 	}
 	if (isset($ranges[$tab])) {
-		$range_min = reset($ranges[$tab]);
-		$range_max = end($ranges[$tab]);
+		$range_min = $ranges[$tab]['min'];
+		$range_max = $ranges[$tab]['max'];
 		foreach ($levels as $k => $v) {
 			if ($k < $range_min) {
 				unset($levels[$k]);
@@ -112,6 +117,10 @@ function Streams_access_tool($options)
 				unset($levels[$k]);
 			}
 		}
+	}
+	if (isset($filter[$tab])) {
+		$keys = array_intersect($filter[$tab], array_keys($levels));
+		$levels = Q::take($levels, $keys);
 	}
 	
 	$accessActionUrl = Q_Uri::url("Streams/access"
